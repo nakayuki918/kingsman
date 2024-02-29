4e4a32f3d1b75719c974519a0b3ba023be936f87
yukinaka <yuki.n.0918@gmail.com>
2024-02-29 16:06:43 +0900

    init

---
+++ main.go
@@ -0,0 +1,47 @@
+package kingsman
+
+import (
+	"log"
+	"os"
+)
+
+package main
+
+import (
+"database/sql"
+"log"
+"os"
+
+"github.com/joho/godotenv"
+_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	// Load connection string from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("failed to load env", err)
+	}
+
+	// Open a connection to PlanetScale
+	db, err := sql.Open("mysql", os.Getenv("DSN"))
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+
+	rows, err := db.Query("SHOW TABLES")
+	if err != nil {
+		log.Fatalf("failed to query: %v", err)
+	}
+	defer rows.Close()
+
+	var tableName string
+	for rows.Next() {
+		if err := rows.Scan(&tableName); err != nil {
+			log.Fatalf("failed to scan row: %v", err)
+		}
+		log.Println(tableName)
+	}
+
+	defer db.Close()
+}
